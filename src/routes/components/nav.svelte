d339758d99ef041ce47f7c24cd8f84a9ebd46bad
Andrew Daniels <danielsuandrew@gmail.com>
2022-07-02 22:38:49 +0000

    first attempt navigation bar

--- src/routes/components/nav.svelte
+++ src/routes/components/nav.svelte
@@ -1,17 +1,76 @@
 <nav>
-    <div class= "header_styles">
-        <a href="/">Home</a> |
-        <a href="/about">About</a> | 
-        <a href="/work">Work</a> |
-        <a href="mailto:danielsuandrew@gmail.com">Contact</a>
+    <div class= "main">
+        <header>
+            <input type="checkbox" name="" id="">
+            <a href="/">Home</a> |
+            <a href="/about">About</a> | 
+            <a href="/work">Work</a> |
+            <a href="mailto:danielsuandrew@gmail.com">Contact</a>
+        </header>
     </div>
 </nav>
 
 <style>
-    .header_styles {
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    .main {
+        width: 100%;
+        height: 100vh;
         display: flex;
         justify-content: center;
-        gap: 1rem;
+        align-items: center;
+    }
+    header{
+        position: relative;
+        width: 70px;
+        height: 25px;
+    }
+    header input{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+        z-index: 5;
+        -webkit-appearance: none;
+        outline: none;
+    }
+    header input::before{
+        content: '\f0c9';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        color: white;
+    }
+    header input:checked::before{
+        content: '\f00d';
+    }
+    header a{
+        position: absolute;
+        opacity: 0;
+        transform: translate(0);
+        color: rgb(20, 35, 108);
+    }
+    header a:hover{
+        color: rgb(7,32,255)
+    }
+    header input:checked ~ a{
+        display: block;
+        opacity: 1;
+    }
+    header input:checked ~ a:nth-child(2){
+        transform: translate(-200px);
+    }
+    header input:checked ~ a:nth-child(3){
+        transform: translate(-100px);
+    }
+    header input:checked ~ a:nth-child(4){
+        transform: translate(100px);
+    }
+    header input:checked ~ a:nth-child(5){
+        transform: translate(210px);
     }
     a {
         color:rgb(20, 35, 108);
